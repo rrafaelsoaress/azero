c90e8e83315bb4217e29a5d3d8ff177d48b8049d
rrafaelsoaress <40873121+rrafaelsoaress@users.noreply.github.com>
2018-07-25 22:12:05 -0300

    include Poltergeist

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,15 @@
 require 'capybara'
 require 'capybara/cucumber'
 require 'site_prism'
+require 'poltergeist'
 
+@browser = ENV['BROWSER']
+
+if @browser.eql?('headless')
+  Capybara.register_driver :selenium do |app|
+    Capybara::Poltergeist::Driver.new(app, { js_errors: false})
+  end
+end
 
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
